8684789e8eea9b3fa7f1830d12285f072122d677
Tom <2470017654@qq.com>
2023-08-10 17:06:30 +0800

    !

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import postcss from 'postcss-preset-env'
 
 export default defineConfig({
   build: {
@@ -6,6 +7,14 @@ export default defineConfig({
       entry: './lib/main.js',
       name: 'Counter',
       fileName: 'counter'
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        // https://juejin.cn/post/7178454300572516409
+        postcss()
+      ]
     }
   }
 })
